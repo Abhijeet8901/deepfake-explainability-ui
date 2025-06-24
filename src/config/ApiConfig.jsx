65fd52d82982a99cd780d0423383e964eca1448a
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-06-24 18:54:02 +1000

    Updated Apis

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,9 +1,9 @@
-export const FAKESHIELD_DTE_BASE_URL = 'https://813ecbd7e538923d77.gradio.live/'
+export const FAKESHIELD_DTE_BASE_URL = 'https://978b00768d8de48cb4.gradio.live'
 
-export const FAKESHIELD_MFLM_BASE_URL = 'https://fc34da30720928b267.gradio.live'
+export const FAKESHIELD_MFLM_BASE_URL = 'https://08db946354ed329756.gradio.live'
 
-export const QWEN_BASE_URL = 'https://cf9f086f8fbd81aedc.gradio.live'
+export const QWEN_BASE_URL = 'https://c908e74be7ad00e5a6.gradio.live'
 
-export const STEP1X_BASE_URL = 'https://0c4d06b4dc9d0db39d.gradio.live'
+export const STEP1X_BASE_URL = 'https://ef1fc660b69065dbe9.gradio.live'
 
 
