9aa96b71f872f8053068f2bcbb8d4198a1e2845f
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-06-25 05:33:14 +1000

    Unified Qwen and Step1X Model

--- src/Redux/Qwen/Action.jsx
+++ src/Redux/Qwen/Action.jsx
@@ -1,4 +1,4 @@
-import { QWEN_BASE_URL } from "../../config/ApiConfig";
+import { STEP1X_BASE_URL } from "../../config/ApiConfig";
 import { RUN_QWEN_FAILURE, RUN_QWEN_REQUEST, RUN_QWEN_SUCCESS } from "./ActionType";
 import { Client } from "@gradio/client";
 
@@ -6,7 +6,7 @@ export const runQwen = (image, complex_explanation) => async (dispatch) => {
   try {
     dispatch({type: RUN_QWEN_REQUEST});
                             
-    const client = await Client.connect(QWEN_BASE_URL);
+    const client = await Client.connect(STEP1X_BASE_URL);
     const result = await client.predict("/simplify", { 
 				img: image, 		
         complex_explanation: complex_explanation, 
--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,9 +1,7 @@
-export const FAKESHIELD_DTE_BASE_URL = 'https://978b00768d8de48cb4.gradio.live'
+export const FAKESHIELD_DTE_BASE_URL = 'https://bee929a81dfc108acc.gradio.live/'
 
 export const FAKESHIELD_MFLM_BASE_URL = 'https://08db946354ed329756.gradio.live'
 
-export const QWEN_BASE_URL = 'https://c908e74be7ad00e5a6.gradio.live'
-
-export const STEP1X_BASE_URL = 'https://ef1fc660b69065dbe9.gradio.live'
+export const STEP1X_BASE_URL = 'https://063b9f8f199c84fc26.gradio.live/'
 
 
