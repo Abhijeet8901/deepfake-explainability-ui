48f603d6ba31a3ec831680cd7547b28fab285b4c
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-07-17 21:21:13 +1000

    Updated Step1X Url

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -2,6 +2,6 @@ export const FAKESHIELD_DTE_BASE_URL = 'https://bee929a81dfc108acc.gradio.live/'
 
 export const FAKESHIELD_MFLM_BASE_URL = 'https://08db946354ed329756.gradio.live'
 
-export const STEP1X_BASE_URL = 'https://a6fd63fe6580934f2b.gradio.live'
+export const STEP1X_BASE_URL = 'https://6d1b33a64ec300097d.gradio.live'
 
 
