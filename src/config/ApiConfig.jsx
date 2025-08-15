871980625255b5d0b594c0c24b3ecede8df75cc2
Abhijeet Narang <abhijeetnarang.nr@gmail.com>
2025-08-15 16:53:06 +1000

    Testing protection rules

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -3,5 +3,3 @@ export const FAKESHIELD_DTE_BASE_URL = 'https://8f6e3b1b3d35ca087f.gradio.live'
 export const FAKESHIELD_MFLM_BASE_URL = 'https://08db946354ed329756.gradio.live'
 
 export const STEP1X_BASE_URL = 'https://6957fd8035c03e5d50.gradio.live'
-
-
